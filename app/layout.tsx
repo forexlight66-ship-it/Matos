40bf762d021fcaab09dbffde0644734b3dd94934
forexlight66-ship-it <forexlight66@gmail.com>
2026-08-11 19:14:17 +0200

    Use full-width layout for MozHyper interface

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,13 +5,9 @@ import { LanguageProvider } from '@/contexts/LanguageContext';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="pt">
       <body>
-        <LanguageProvider>
-          <main className="container mx-auto px-4 py-8 max-w-6xl">
-            {children}
-          </main>
-        </LanguageProvider>
+        <LanguageProvider>{children}</LanguageProvider>
       </body>
     </html>
   );
