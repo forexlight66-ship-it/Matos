11ba676938d4c2d58f7e72f337d0283afc301185
forexlight66-ship-it <forexlight66@gmail.com>
2026-08-16 08:39:12 +0200

    Show 100-tick digit probabilities in dashboard

--- components/Dashboard.tsx
+++ components/Dashboard.tsx
@@ -4,6 +4,7 @@
 
 import TutorialSection from './TutorialSection';
 import DigitsGameV3 from './DigitsGameV3';
+import ProbabilitySampling100 from './ProbabilitySampling100';
 
 export default function Dashboard() {
   return (
@@ -11,6 +12,7 @@ export default function Dashboard() {
       <div className="matos-shell">
         <div className="matos-phone">
           <DigitsGameV3 />
+          <ProbabilitySampling100 />
         </div>
 
         <div id="tutorial" className="mt-4">
