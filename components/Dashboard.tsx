886efd9bb690dc2c590a59748feeefa1561bf7fb
forexlight66-ship-it <forexlight66@gmail.com>
2026-08-28 08:04:30 +0200

    Use fixed automatic bot with immediate Soros continuation

--- components/Dashboard.tsx
+++ components/Dashboard.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from 'react';
 import TutorialSection from './TutorialSection';
-import AutoBotV3 from './AutoBotV3';
+import AutoBotV4 from './AutoBotV4';
 import RobotStrategySelector from './RobotStrategySelector';
 import AutoBotEnhancements from './AutoBotEnhancements';
 
@@ -36,7 +36,7 @@ export default function Dashboard() {
           <button className={mode==='robot'?'active robo':''} onClick={()=>{setMode('robot');setMenu(false)}}>🤖 Robô</button>
         </div>
         <AutoBotEnhancements />
-        {mode==='automatic'?<AutoBotV3/>:<RobotStrategySelector/>}
+        {mode==='automatic'?<AutoBotV4/>:<RobotStrategySelector/>}
       </div>
       <div id="tutorial" className="mt-4"><TutorialSection/></div>
     </div>
